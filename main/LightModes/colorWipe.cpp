d1de2a615db5d29e48f95a06fe1ceb224ca8fa8e
Kyrel Jerome <kyrel2007@gmail.com>
2019-06-15 14:50:06 -0400

    configured lights in OOP

---
+++ main/LightModes/colorWipe.cpp
@@ -0,0 +1,40 @@
+//#include <Adafruit_NeoPixel.h>
+#include "strategy.h"
+class WipeModeStrategy : public LightModeStrategy
+{
+public:
+    bool hasWiped;
+    bool hasSetup;
+    uint32_t colour;
+    WipeModeStrategy(uint32_t colour, int numLights): LightModeStrategy(numLights)
+    {
+        this->hasWiped = false;
+        this->colour = colour;
+    }
+    void disable()
+    {
+        hasWiped = false;
+    }
+
+protected:
+    void setup()
+    {
+        this->hasSetup = true;
+    }
+    bool shouldSetup()
+    {
+        return true;
+    }
+    bool shouldUpdate()
+    {
+        return !this->hasWiped;
+    }
+    void _update(Adafruit_NeoPixel strip)
+    {
+        for (int i = 0; i < numLights; i++)
+        {
+            strip.setPixelColor(i, colour);
+        }
+        this->hasWiped = true;
+    }
+};
---
+++ main/LightModes/strategy.h
@@ -0,0 +1,30 @@
+#include <Adafruit_NeoPixel.h>
+class LightModeStrategy
+{
+public:
+    int numLights;
+    LightModeStrategy(int nLights)
+    {
+        numLights = nLights;
+    }
+
+    void update(Adafruit_NeoPixel strip)
+    {
+        if (shouldSetup())
+        {
+            setup();
+        }
+        if (shouldUpdate())
+        {
+            _update(strip);
+        }
+    }
+
+    virtual void disable() = 0;
+
+protected:
+    virtual void setup() = 0;
+    virtual bool shouldSetup() = 0;
+    virtual bool shouldUpdate() = 0;
+    virtual void _update(Adafruit_NeoPixel strip) = 0;
+};
