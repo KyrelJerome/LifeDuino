8128f3e4704db899d4df0558ff65cb7d918bc5b4
Kyrel Jerome <kyrel2007@gmail.com>
2019-07-15 17:44:57 -0400

    Integrated modules

--- main/main.ino
+++ main/main.ino
@@ -5,13 +5,9 @@
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
-//OUTPUT PINS
-#define DESK_PIN 6
 
-//INPUT PINS
-#define RIO_PINA 4
-#define RIO_PINB 3
-#define TOGGLE_PIN 2
+//OUTPUT PINS
+#define DESK_PIN 13
 
 // DEFINE LIGHT MODES
 #define MODE_GREEN 0
@@ -26,37 +22,30 @@
 #define YELLOW stripDesk.Color(255, 255, 0)
 #define OFF stripDesk.Color(0, 0, 0)
 
+#define NUM_MODES_DESK 5
 #define NUM_LIGHTS_DESK 200
 #define BRIGHTNESS_DESK 50
 
-int AState;
-int BState;
+
 int toggleState;
 int currentMode;
 int lastMode;
-WipeModeStrategy WIPEWHITE = WipeModeStrategy(WHITE, NUM_LIGHTS_DESK);
-WipeModeStrategy WIPERED = WipeModeStrategy(RED, NUM_LIGHTS_DESK);
-WipeModeStrategy WIPEGREEN = WipeModeStrategy(GREEN, NUM_LIGHTS_DESK);
-WipeModeStrategy WIPEBLUE = WipeModeStrategy(BLUE, NUM_LIGHTS_DESK);
+
 int NUM_LIGHT_MODES = 5;
 LightModeStrategy* deskModes[4] = {
   &WIPEBLUE, &WIPEWHITE, &WIPERED,
   &WIPEGREEN};
-LightingModule computerDesk = LightingModule(NUM_LIGHTS_DESK);
-void setup()
+LightingModule computerDesk = void setup()
 {
   Serial.begin(9600);
-  // put your setup code here, to run once:
-  //Sets RIO related pins to INPUT
-  pinMode(RIO_PINA, INPUT);
-  pinMode(RIO_PINB, INPUT);
-  pinMode(TOGGLE_PIN, INPUT);
   pinMode(DESK_PIN, OUTPUT);
   stripDesk.setBrightness(BRIGHTNESS_DESK);
-
   lastMode = -1;
   currentMode = MODE_RED;
+  computerDesk = LightingModule(NUM_LIGHTS_DESK, DESK_PIN, lightModes, getDeskModes());
+
 }
+
 
 void loop()
 {
@@ -65,6 +54,7 @@ void loop()
   update();
   stripDesk.show();
 }
+
 void updateInputs()
 {
   AState = digitalRead(RIO_PINA);
@@ -86,7 +76,6 @@ void updateMode()
   {
     currentMode = MODE_RED;
   }
-
   else if (AState == 0 && BState == 0)
   {
     currentMode = MODE_GREEN;
@@ -105,4 +94,17 @@ void update()
   deskModes[currentMode]->update(stripDesk);
   tvModes[tvMode]->update(stripTV);
   lastMode = currentMode;
+}
+
+LightModeStrategy* getModes(){
+  LightModeStrategy* modes = malloc(sizeof(LightModeStrategy*)*NUM_MODES_DESK);
+  for(int i = 0; i < NUM_MODES_DESK; i ++){
+      modes[i] = malloc(sizeof(LightModeStrategy));
+  }
+  modes[0] = WipeModeStrategy(WHITE, NUM_LIGHTS_DESK);
+  modes[1] = WipeModeStrategy(RED, NUM_LIGHTS_DESK);
+  modes[2] = WipeModeStrategy(GREEN, NUM_LIGHTS_DESK);
+  modes[3] = WipeModeStrategy(BLUE, NUM_LIGHTS_DESK);
+  modes[4] = WipeModeStrategy(OFF, NUM_LIGHTS_DESK);
+  return modes;
 }
